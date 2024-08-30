5cede1a2e005ecf5f24d88b2ee3c8810a3264314
EXELVI <exelvi@outlook.com>
2024-08-30 11:03:29 +0200

    chore: Add collaborative text editing functionality

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const io = new Server(server);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+const rooms = {};
+
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -18,13 +20,30 @@ io.on('connection', (socket) => {
 
     socket.on('joinRoom', (room) => {
         socket.join(room);
+
+        if (rooms[room]) {
+            socket.emit('textChange', rooms[room].text);
+        } else {
+            rooms[room] = { text: '', cursors: {} };
+        }
+
         socket.on('textChange', (data) => {
+            rooms[room].text = data;
             socket.to(room).emit('textChange', data);
         });
-    });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+        socket.on('cursorChange', (cursorData) => {
+            rooms[room].cursors[socket.id] = cursorData;
+            socket.to(room).emit('cursorChange', { id: socket.id, ...cursorData });
+        });
+
+        socket.on('disconnect', () => {
+            if (rooms[room]) {
+                delete rooms[room].cursors[socket.id];
+                socket.to(room).emit('cursorRemove', socket.id);
+            }
+            console.log('user disconnected');
+        });
     });
 });
 
